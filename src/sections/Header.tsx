f8760f23a1e17612102cd5740081753075d6b335
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-02-14 17:24:49 -0300

    style: adding gutter in the containers

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,20 +4,24 @@ import { DropdownLanguage } from '../components/DropdownLanguage'
 export function Header() {
   return (
     <header className="bg-neutral-900 text-white py-10">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-6xl text-orange-400 font-logo select-none">ک</div>
-        <nav className="flex gap-14">
-          <a className="hover:text-orange-300 transition-all" href="#">
-            Apresentação
-          </a>
-          <a className="hover:text-orange-300 transition-all" href="#">
-            Projetos
-          </a>
-          <DropdownLanguage>
-            <Translate size={16} weight="fill" />
-            Português
-          </DropdownLanguage>
-        </nav>
+      <div className="container mx-auto px-8">
+        <div className="flex justify-between items-center">
+          <div className="text-6xl text-orange-400 font-logo select-none">
+            ک
+          </div>
+          <nav className="flex gap-14">
+            <a className="hover:text-orange-300 transition-all" href="#">
+              Apresentação
+            </a>
+            <a className="hover:text-orange-300 transition-all" href="#">
+              Projetos
+            </a>
+            <DropdownLanguage>
+              <Translate size={16} weight="fill" />
+              Português
+            </DropdownLanguage>
+          </nav>
+        </div>
       </div>
     </header>
   )
