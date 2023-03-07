90fa7deba2bba25cb6e1e52614a5017dbd63d62a
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-03-07 11:21:43 -0300

    fix: new format for description

--- src/libs/locales/en-us.ts
+++ src/libs/locales/en-us.ts
@@ -7,7 +7,11 @@ export default {
     currentLanguage: 'English',
 
     title: 'Who am I?',
-    description:
+    description1:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros nibh, convallis molestie eleifend suscipit, iaculis at turpis. Maecenas volutpat venenatis velit eu finibus. Praesent enim neque, ultrices non imperdiet in, sagittis a mi. Nullam non ex vitae est molestie luctus. Morbi vel cursus ex, nec sodales odio. Integer tempor est vel lorem finibus, nec ullamcorper diam ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
+    description2:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros nibh, convallis molestie eleifend suscipit, iaculis at turpis. Maecenas volutpat venenatis velit eu finibus. Praesent enim neque, ultrices non imperdiet in, sagittis a mi. Nullam non ex vitae est molestie luctus. Morbi vel cursus ex, nec sodales odio. Integer tempor est vel lorem finibus, nec ullamcorper diam ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
+    description3:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros nibh, convallis molestie eleifend suscipit, iaculis at turpis. Maecenas volutpat venenatis velit eu finibus. Praesent enim neque, ultrices non imperdiet in, sagittis a mi. Nullam non ex vitae est molestie luctus. Morbi vel cursus ex, nec sodales odio. Integer tempor est vel lorem finibus, nec ullamcorper diam ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;',
 
     technologiesUsed: 'Technologies Used',
