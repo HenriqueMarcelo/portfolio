abfde1c78ea525622bbe01222e01da3a9351f1dd
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-03-10 14:24:19 -0300

    ci: creating redirects

---
+++ public/coffee-delivery/index.php
@@ -0,0 +1,7 @@
+<?php
+
+header("Location: https://vocab-plus-plus.kursi.com.br/", true, 301);
+
+exit();
+
+?>
---
+++ public/vocab-plus-plus/index.php
@@ -0,0 +1,7 @@
+<?php
+
+header("Location: https://coffee-delivery.kursi.com.br/", true, 301);
+
+exit();
+
+?>
